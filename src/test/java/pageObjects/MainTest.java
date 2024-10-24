bc0134baf820ac4b054fdd71be10caa0fda462a3
varun <jm.varun222@gmail.com>
2024-10-25 01:39:23 +0530

    this is my first commit

--- src/test/java/pageObjects/MainTest.java
+++ src/test/java/pageObjects/MainTest.java
@@ -3,14 +3,11 @@ package pageObjects;
 
 import VarunHasToWork.BaseTest;
 
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
+
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
